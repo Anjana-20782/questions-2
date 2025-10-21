a8a55a23821782636756d389be930f67fe6ec03b
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-21 17:31:23 +0530

    new contents added

---
+++ js/questions/primenumbers(1-100).js
@@ -0,0 +1,17 @@
+for(i=2;i<=100;i++)
+{
+    let count=0;
+    for(let j=1;j<=i;j++){
+
+        if(i%j===0)
+        {
+            count++
+        }
+    }
+
+    if(count==2)
+    {
+        console.log("prime number:"+i);
+        
+    }
+}
